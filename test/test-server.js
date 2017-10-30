21b23bad06d871287e7d332f9217856c0656c961
dermio <thisdjay@gmail.com>
2017-10-29 19:55:51 -0700

    Added it test for GET route for recipes.

--- test/test-server.js
+++ test/test-server.js
@@ -149,5 +149,24 @@ describe("Recipes", function () {
     return closeServer();
   });
 
-  
+
+  it("list recipes on GET", function () {
+    return chai.request(app)
+      .get("/recipes")
+      .then(function (res) {
+        // console.log(res);
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a("array");
+
+        res.body.length.should.be.at.least(1);
+
+        let expectedKeys = ["id", "name", "ingredients"];
+        res.body.forEach(function (item) {
+          item.should.be.a("object");
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
 });
